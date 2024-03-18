1210fef2029f7653614d57d421e89a78bbb9b6a1
Gaurav Sablok <sablokg@gmail.com>
2024-03-18 10:56:41 +0100

    Update visualfreq.R

--- visualfreq.R
+++ visualfreq.R
@@ -1,6 +1,19 @@
 #!/usr/bin/R
+# Author Gaurav Sablok
+# Universitat Potsdam
+# Date 2024-3-18
+# a gene alignment to the visual alignment priting 
+# for the creation of the alignment maps
 library(ggplots2)
 library(BioStrings)
-visualfreq <- function(inputfile){
-  
+library(ape)
+library(msa)
+visualfreq <- function(inputfile, alignmentwidth){
+  if (!stopifnot(inputfile)){
+    paste0("inputfile is necessary)"
+  } else
+  filefasta <- read.FASTA(inputfile)
+  multiplealignments <- msa(filefasta)
+  imagevisual <- ggmsa(multiplealignment, width = alignmentwidth)
+  save.image(file = "alignmentvisual", compress = FALSE)         
 }
