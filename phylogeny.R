9b2fbed6efb0f818fd4a8f3d706aa2499e676ec9
Gaurav Sablok <sablokg@gmail.com>
2024-03-18 11:01:28 +0100

    Update phylogeny.R

--- phylogeny.R
+++ phylogeny.R
@@ -1,8 +1,14 @@
-phylogeny () {
-   # a streamline function for accessing the ENA archives using the API access.
-   #!/usr/bin/env bash
-   # -*- coding:  utf-8 -*-
-   # Author: Gaurav Sablok
-   # date: 2024-3-18
-
+#! usr/bin/R
+# Author Gaurav Sablok
+# Universitat Potsdam
+# making the phylogeny and plotting the alignment on the phylogeny using the ggtree and the ggplot2
+library(Biostrings)
+library(ggplots2)
+library(ggtree)
+alignmentToPhylogeny <- function(inputalignmentfile, option){
+   if (!inputfile) && (!option){
+      paste0("the function needs an alignment file and the option)"
+   } 
+   else 
+      alignmentfile <- read.FASTA(inputalignmentfile)
 }
