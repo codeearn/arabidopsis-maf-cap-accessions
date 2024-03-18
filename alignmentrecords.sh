cf337512c63e6b8e4aa1e22379e90b00fa6acea6
Gaurav Sablok <sablokg@gmail.com>
2024-03-18 10:43:50 +0100

    Update alignmentrecords.sh

--- alignmentrecords.sh
+++ alignmentrecords.sh
@@ -1,9 +1,10 @@
+  GNU nano 6.2                                                    alignmentrecord.sh *                                                     M     
 alignment () {
    # a streamline function for accessing the ENA archives using the API access.
    #!/usr/bin/env bash
    # -*- coding:  utf-8 -*-
    # Author: Gaurav Sablok
-   # date: 2024-3-16
+   # date: 2024-3-18
    echo "process the alignment"
    echo "checking the alignment tags"
    echo "making the environment"
@@ -12,5 +13,20 @@ alignment () {
    sudo apt-get install iqtree
    sudo apt-get install raxml-ng
    sudo apt-get install model-ng
-  
-}
+   mkdir $1 
+   cd $1 
+   for i in *.fasta; do muscle -in $i -out ${i%.}.aligned.fasta 
+   for i in ${i%.*}.aligned.fasta; do trimal -in $i -out ${i%.**}.trim.fasta --nogaps
+   for i in ${i%.**}.trim.fasta; do  readal -in $i -out ${i%.**}.stats 
+   echo "thank you for using the alignments"
+   echo "muscle alignments have been done" 
+   echo "alignments have been trimmed using the trimal" 
+   echo "alignment visualization have been generated using the readal" 
+} << $1 
+
+
+
+
+
+
+
