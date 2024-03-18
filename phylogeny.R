cc88fa174ba404ba1885cc123c440b8d48d6fade
Gaurav Sablok <sablokg@gmail.com>
2024-03-18 13:57:22 +0100

    Update phylogeny.R

--- phylogeny.R
+++ phylogeny.R
@@ -2,13 +2,22 @@
 # Author Gaurav Sablok
 # Universitat Potsdam
 # making the phylogeny and plotting the alignment on the phylogeny using the ggtree and the ggplot2
-library(Biostrings)
-library(ggplots2)
-library(ggtree)
+library(Biostrings, suppressPackageStartupMessages())
+library(ggplots2, suppressPackageStartupMessages())
+library(ggtree, suppressPackageStartupMessages())
 alignmentToPhylogeny <- function(inputalignmentfile, option){
    if (!inputfile) && (!option){
       paste0("the function needs an alignment file and the option)"
    } 
    else 
-      alignmentfile <- read.FASTA(inputalignmentfile)
+      alignmentfile <- readDNAStringset(inputfile)
+      aligniningregions <- msa(alignmentfile)
+      convertalignment <- msaconvert(aligningregions, format= "FASTA")
+      write.fasta(convertalignment, file = "aligned.fasta")
+      paste0("reading the aligned file" + "getwd()/aligned.fasta")
+      readingalignment <- readDNAAlignment("aligned.fasta")
+      ggmsa(readingalignment, 180, 300, colour=rgb)
+      return (readingalingment)
 }
+###### argument parser version #######
+library(argparse)
