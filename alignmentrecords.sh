330751d516c7a470a7552c529faf91f1e37a5656
Gaurav Sablok <sablokg@gmail.com>
2024-03-18 10:02:59 +0100

    Create alignmentrecords.sh

---
+++ alignmentrecords.sh
@@ -0,0 +1,16 @@
+alignment {
+   # a streamline function for accessing the ENA archives using the API access.
+   #!/usr/bin/env bash
+   # -*- coding:  utf-8 -*-
+   # Author: Gaurav Sablok
+   # date: 2024-3-16
+   echo "process the alignment"
+   echo "checking the alignment tags"
+   echo "making the environment"
+   sudo apt-get install lastz
+   sudo apt-get install muscle
+   sudo apt-get install iqtree
+   sudo apt-get install raxml-ng
+   sudo apt-get install model-ng
+  
+}
